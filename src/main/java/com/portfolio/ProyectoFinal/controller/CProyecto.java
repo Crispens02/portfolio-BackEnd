08ef20398a595506ee158da84ed1f0973b0b5f8f
Cristian <krhyz02@gmail.com>
2023-01-07 17:09:26 -0300

    URL actualizados

--- src/main/java/com/portfolio/ProyectoFinal/controller/CProyecto.java
+++ src/main/java/com/portfolio/ProyectoFinal/controller/CProyecto.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendcrispens02.web.app/")
 public class CProyecto {
     @Autowired
     ProyectoService sProyecto;
--- src/main/java/com/portfolio/ProyectoFinal/controller/PersonaController.java
+++ src/main/java/com/portfolio/ProyectoFinal/controller/PersonaController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendcrispens02.web.app/")
 public class PersonaController {
   @Autowired
     ImpPersonaService personaService;
